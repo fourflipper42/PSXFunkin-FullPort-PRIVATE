2801e8591167f170f5f5cbbdc6593c098be29976
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 04:49:17 -0400

    PS2: add gameplay lane renderer API

---
+++ ps2/src/core/note_lane_renderer.h
@@ -0,0 +1,15 @@
+#ifndef FNF_PS2_NOTE_LANE_RENDERER_H
+#define FNF_PS2_NOTE_LANE_RENDERER_H
+
+#include "gameplay.h"
+#include "note_style.h"
+
+void NoteLaneRenderer_Reset(void);
+void NoteLaneRenderer_Tick(const GameplayState *game, const Pad *pad);
+void NoteLaneRenderer_Draw(
+    GSGLOBAL *gs,
+    const NoteStyle *style,
+    const GameplayState *game,
+    const Pad *pad);
+
+#endif
