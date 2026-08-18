487659d3daf50d2f840e00b27d784c276fc21ab2
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 02:04:31 -0400

    Add portable PS2 gameplay timer interface

---
+++ ps2/src/core/timer.h
@@ -0,0 +1,15 @@
+#ifndef FNF_PS2_TIMER_H
+#define FNF_PS2_TIMER_H
+
+#include "fixed.h"
+
+extern u32 frame_count;
+extern u32 animf_count;
+extern fixed_t timer_sec;
+extern fixed_t timer_dt;
+
+void Timer_Init(void);
+void Timer_Tick(void);
+void Timer_Reset(void);
+
+#endif
