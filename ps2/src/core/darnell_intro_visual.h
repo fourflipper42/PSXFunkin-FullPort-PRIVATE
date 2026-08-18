2f9c288740a4ffd2221533c8d7ca3be4b005546a
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 16:58:18 -0400

    PS2: add Darnell intro can visual API

---
+++ ps2/src/core/darnell_intro_visual.h
@@ -0,0 +1,19 @@
+#ifndef FNF_PS2_DARNELL_INTRO_VISUAL_H
+#define FNF_PS2_DARNELL_INTRO_VISUAL_H
+
+#include "stage.h"
+
+void DarnellIntroVisual_Begin(void);
+void DarnellIntroVisual_End(void);
+void DarnellIntroVisual_KickUp(void);
+void DarnellIntroVisual_KneeForward(void);
+void DarnellIntroVisual_Shoot(void);
+void DarnellIntroVisual_Tick(fixed_t elapsed);
+void DarnellIntroVisual_DrawRange(
+    GSGLOBAL *gs,
+    const Stage *stage,
+    const StageCamera *camera,
+    s32 z_min,
+    s32 z_max);
+
+#endif
