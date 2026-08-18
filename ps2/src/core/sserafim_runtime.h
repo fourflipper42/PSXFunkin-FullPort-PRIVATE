7dab0c1448d1d5a60c1359f08c675955161a071f
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 17:25:38 -0400

    PS2: add Sserafim stage runtime API

---
+++ ps2/src/core/sserafim_runtime.h
@@ -0,0 +1,25 @@
+#ifndef FNF_PS2_SSERAFIM_RUNTIME_H
+#define FNF_PS2_SSERAFIM_RUNTIME_H
+
+#include "gameplay.h"
+#include "stage.h"
+
+boolean SserafimRuntime_Begin(GSGLOBAL *gs, Stage *stage, const char *base_path);
+void SserafimRuntime_End(void);
+boolean SserafimRuntime_Active(void);
+void SserafimRuntime_Tick(fixed_t elapsed);
+void SserafimRuntime_Beat(s32 beat);
+void SserafimRuntime_DrawRange(
+    GSGLOBAL *gs,
+    const StageCamera *camera,
+    s32 z_min,
+    s32 z_max);
+boolean SserafimRuntime_HandleEvent(const char *name, const char *value);
+void SserafimRuntime_PlayHitAnimations(
+    const GameplayState *game,
+    Character *player,
+    Character *opponent,
+    Character *girlfriend);
+void SserafimRuntime_DrawOverlay(GSGLOBAL *gs);
+
+#endif
