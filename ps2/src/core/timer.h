12e026234523e4d5847351159ff457ada35fb9ff
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 08:53:59 -0400

    PS2: add presentation-rate frame delta

--- ps2/src/core/timer.h
+++ ps2/src/core/timer.h
@@ -7,6 +7,9 @@ extern u32 frame_count;
 extern u32 animf_count;
 extern fixed_t timer_sec;
 extern fixed_t timer_dt;
+/* Presentation-only delta. Reset to timer_dt each frame, then gameplay mods
+ * may scale it before character/stage animation ticks run. */
+extern fixed_t timer_presentation_dt;
 
 void Timer_Init(void);
 void Timer_Tick(void);
