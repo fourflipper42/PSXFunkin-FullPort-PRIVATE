f46a03f3b761b0169306836de2a8e74ca2f94bca
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 17:29:06 -0400

    PS2: route Sserafim singer animations

---
+++ ps2/src/core/session_runtime_hits.c
@@ -0,0 +1,29 @@
+#define SESSION_RUNTIME_IMPLEMENTATION
+#include "session_runtime.h"
+
+#include "sserafim_runtime.h"
+
+void SessionRuntime_PlayHitAnimationsCore(
+    SessionRuntime *session,
+    Character *player,
+    Character *opponent);
+
+void SessionRuntime_PlayHitAnimations(
+    SessionRuntime *session,
+    Character *player,
+    Character *opponent)
+{
+    if (session == NULL || !session->song_active)
+        return;
+
+    if (SserafimRuntime_Active()) {
+        SserafimRuntime_PlayHitAnimations(
+            session->active_game,
+            player,
+            opponent,
+            PresentationRegistry_Girlfriend());
+        return;
+    }
+
+    SessionRuntime_PlayHitAnimationsCore(session, player, opponent);
+}
