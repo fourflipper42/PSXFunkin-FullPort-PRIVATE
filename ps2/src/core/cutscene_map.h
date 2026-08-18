905c51c804519c88ad295b42b79d64c30d112af8
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 10:45:50 -0400

    PS2: expose ending cutscene map entries

--- ps2/src/core/cutscene_map.h
+++ ps2/src/core/cutscene_map.h
@@ -5,6 +5,7 @@
 
 #define CUTSCENE_MAP_FLAG_PRESONG     (1u << 0)
 #define CUTSCENE_MAP_FLAG_STORY_ONLY  (1u << 1)
+#define CUTSCENE_MAP_FLAG_POSTSONG    (1u << 2)
 
 typedef struct CutsceneMapEntry {
     u32 song_offset;
@@ -25,6 +26,11 @@ typedef struct CutsceneMap {
 boolean CutsceneMap_Load(CutsceneMap *map, const char *path);
 void CutsceneMap_Free(CutsceneMap *map);
 boolean CutsceneMap_FindPreSong(
+    const CutsceneMap *map,
+    const char *song_id,
+    boolean story_mode,
+    const char **cutscene_id);
+boolean CutsceneMap_FindPostSong(
     const CutsceneMap *map,
     const char *song_id,
     boolean story_mode,
