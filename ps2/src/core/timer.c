426d62a2f6d00aaa8b630dcc95c76e6567b4e60c
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 08:54:13 -0400

    PS2: reset presentation delta every VSync

--- ps2/src/core/timer.c
+++ ps2/src/core/timer.c
@@ -4,6 +4,7 @@ u32 frame_count;
 u32 animf_count;
 fixed_t timer_sec;
 fixed_t timer_dt;
+fixed_t timer_presentation_dt;
 
 void Timer_Init(void)
 {
@@ -11,6 +12,7 @@ void Timer_Init(void)
     animf_count = 0;
     timer_sec = 0;
     timer_dt = 0;
+    timer_presentation_dt = 0;
 }
 
 void Timer_Tick(void)
@@ -22,12 +24,12 @@ void Timer_Tick(void)
     /*
      * The bootstrap renderer is VSync-paced at NTSC 60 Hz. Deriving time from
      * the frame count rather than repeatedly adding 1/60 avoids fixed-point
-     * rounding drift. Song position will later come from the audio stream,
-     * exactly where rhythm timing belongs.
+     * rounding drift. Song position comes from the audio stream.
      */
     next_sec = FIXED_DIV((fixed_t)frame_count, 60);
     timer_dt = next_sec - timer_sec;
     timer_sec = next_sec;
+    timer_presentation_dt = timer_dt;
     animf_count = (timer_sec * 24) >> FIXED_SHIFT;
 }
 
