567462b977b2d26ae17727e6f15988794d646a60
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 08:10:47 -0400

    PS2: add variable-rate PCM song streaming

--- ps2/src/core/song_stream.h
+++ ps2/src/core/song_stream.h
@@ -12,14 +12,21 @@ typedef struct SongStream {
     boolean active;
     boolean finished;
     boolean paused;
+    boolean source_eof;
     u64 base_frame;
     u64 paused_frame;
+    fixed_t playback_rate;
+    u32 source_phase;
+    u32 source_buffer_frames;
+    u32 source_buffer_cursor;
 } SongStream;
 
 boolean SongStream_Open(SongStream *stream, const char *inst_path, const char *voices_path);
 void SongStream_Close(SongStream *stream);
 void SongStream_Tick(SongStream *stream);
 void SongStream_SetVoices(SongStream *stream, boolean enabled);
+boolean SongStream_SetPlaybackRate(SongStream *stream, fixed_t rate);
+fixed_t SongStream_PlaybackRate(const SongStream *stream);
 boolean SongStream_SeekFrame(SongStream *stream, u64 frame);
 boolean SongStream_Pause(SongStream *stream);
 boolean SongStream_Resume(SongStream *stream);
