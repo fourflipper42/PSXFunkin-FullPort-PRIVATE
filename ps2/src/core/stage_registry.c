eda0b7acc516b1c13b32617f7c7a741f730111d4
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 16:54:05 -0400

    PS2: draw Weekend 1 visuals in stage layers

--- ps2/src/core/stage_registry.c
+++ ps2/src/core/stage_registry.c
@@ -1,9 +1,16 @@
 #include "stage.h"
 
 #include "presentation_registry.h"
+#include "weekend1_visual.h"
 
 boolean Stage_LoadCore(GSGLOBAL *gs, Stage *stage, const char *base_path);
 void Stage_ForgetCore(Stage *stage);
+void Stage_DrawRangeCore(
+    GSGLOBAL *gs,
+    const Stage *stage,
+    const StageCamera *camera,
+    s32 z_min,
+    s32 z_max);
 
 boolean Stage_Load(GSGLOBAL *gs, Stage *stage, const char *base_path)
 {
@@ -19,4 +26,15 @@ void Stage_Forget(Stage *stage)
 {
     PresentationRegistry_UnregisterStage(stage);
     Stage_ForgetCore(stage);
+}
+
+void Stage_DrawRange(
+    GSGLOBAL *gs,
+    const Stage *stage,
+    const StageCamera *camera,
+    s32 z_min,
+    s32 z_max)
+{
+    Stage_DrawRangeCore(gs, stage, camera, z_min, z_max);
+    Weekend1Visual_DrawRange(gs, stage, camera, z_min, z_max);
 }
