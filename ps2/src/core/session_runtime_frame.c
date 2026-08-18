4e70a47f753a3e857c7c2e50c8d45080701e09fe
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 10:39:03 -0400

    PS2: tick cutscene gate before gameplay presentation

--- ps2/src/core/session_runtime_frame.c
+++ ps2/src/core/session_runtime_frame.c
@@ -1,11 +1,19 @@
 #define SESSION_RUNTIME_IMPLEMENTATION
 #include "session_runtime.h"
 
+#include "cutscene_controller.h"
+
 void SessionRuntime_AfterGameplayFrameScaled(
     SessionRuntime *session,
     GameplayState *game,
     fixed_t elapsed)
 {
+    if (CutsceneController_Active()) {
+        timer_presentation_dt = 0;
+        CutsceneController_Tick();
+        return;
+    }
+
     if (session != NULL && session->song_active)
         timer_presentation_dt = Gammod_PresentationDelta(&session->gammod, elapsed);
     else
