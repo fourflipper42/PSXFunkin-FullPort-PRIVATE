14cd0a3a7c801a5435b41594665084c66bbbcf47
fourflipper42 <deltacarro92@gmail.com>
2026-08-18 02:11:43 -0400

    Add portable FNF rhythm state

---
+++ ps2/src/core/rhythm.h
@@ -0,0 +1,46 @@
+#ifndef FNF_PS2_RHYTHM_H
+#define FNF_PS2_RHYTHM_H
+
+#include "fixed.h"
+
+typedef enum HitRating {
+    HIT_SICK = 0,
+    HIT_GOOD = 1,
+    HIT_BAD = 2,
+    HIT_SHIT = 3
+} HitRating;
+
+typedef struct RhythmState {
+    boolean kade;
+    boolean ghost;
+    fixed_t speed;
+
+    u16 last_bpm;
+    u16 step_base;
+    fixed_t time_base;
+    fixed_t step_crochet;
+    fixed_t early_safe;
+    fixed_t late_safe;
+    fixed_t early_sus_safe;
+    fixed_t late_sus_safe;
+    fixed_t note_speed;
+
+    s16 health;
+    u16 combo;
+    s32 score;
+} RhythmState;
+
+typedef struct RhythmHitResult {
+    HitRating rating;
+    boolean start_vocal;
+    boolean cut_vocal;
+    boolean combo_broken;
+} RhythmHitResult;
+
+void Rhythm_Init(RhythmState *state, boolean kade, boolean ghost, fixed_t speed);
+void Rhythm_ChangeBPM(RhythmState *state, u16 bpm, u16 step);
+HitRating Rhythm_ClassifyHit(const RhythmState *state, fixed_t offset);
+RhythmHitResult Rhythm_ApplyHit(RhythmState *state, fixed_t offset);
+boolean Rhythm_ApplyEmptyPressMiss(RhythmState *state);
+
+#endif
